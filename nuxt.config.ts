304eed43e392b99e870f2099364ae61973ddf04b
liusu <1421222481@qq.com>
2024-06-03 14:24:20 +0000

    增加uno配置

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@ant-design-vue/nuxt"],
+  modules: ["@ant-design-vue/nuxt", "@unocss/nuxt"],
   devtools: { enabled: true },
 });
